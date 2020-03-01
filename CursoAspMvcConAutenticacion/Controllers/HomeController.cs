878b1f68dc7082f25fb60335943fa2ac5b1fb710
ahcruzado <ahcruzado@users.noreply.github.com>
2020-03-01 19:14:27 -0300

    como crear vista vacía

--- CursoAspMvcConAutenticacion/Controllers/HomeController.cs
+++ CursoAspMvcConAutenticacion/Controllers/HomeController.cs
@@ -56,6 +56,9 @@ namespace CursoAspMvcConAutenticacion.Controllers
             return View();
         }
 
-
+        public ActionResult MiAction()
+        {
+            return View();
+        }
     }
 }
